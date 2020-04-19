c56a02ace14b651031fded5bf11c3ee867937e27
Artyom Izotov <ezik5353ezik@gmail.com>
2020-04-19 22:59:44 +0300

    Code: перенёс все функции с svg в hisoram.cpp

--- historam.cpp
+++ historam.cpp
@@ -1,4 +1,5 @@
 #include "histogram.h"
+#include <iostream>
 void find_minmax(vector<double> numbers, double& min, double& max)
 {
     min = numbers[0];
@@ -15,4 +16,65 @@ void find_minmax(vector<double> numbers, double& min, double& max)
         }
     }
     // (здесь код поиска минимума и максимума)
+}
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+void svg_rect(double x, double y, double width, double height,string stroke,string fill)
+{
+    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+void svg_text(double left, double baseline, string text,size_t bin)
+{
+    cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<bin<<"</text>";
+}
+void show_histogram_svg(const vector<size_t> bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    // svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    //svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+    double top = 0;
+    string stroke="black";
+    string fill="red";
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+    for (size_t bin : bins)
+    {
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            bin = (size_t)(bin * scaling_factor);
+        }
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin),bin);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,stroke,fill);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
 }
