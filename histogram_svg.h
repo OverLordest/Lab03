889a20f291dd96a516e8e5d3eebdd6ee4ce3141d
Artyom Izotov <ezik5353ezik@gmail.com>
2020-04-20 17:13:17 +0300

    Code: добавил все функции svg в хэдер

--- histogram_svg.h
+++ histogram_svg.h
@@ -1,8 +1,13 @@
 #ifndef HISTOGRAM_SVG_H_INCLUDED
 #define HISTOGRAM_SVG_H_INCLUDED
 #include <vector>
+#include <iostream>
 using namespace std;
 void show_histogram_svg(const vector<size_t> bins,size_t number_count);
+void svg_begin(double width, double height);
+void svg_end();
+void svg_rect(double x, double y, double width, double height,string stroke,string fill);
+void svg_text(double left, double baseline, string text,size_t bin);
 
 
 #endif // HISTOGRAM_SVG_H_INCLUDED
