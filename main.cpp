2c6d0393326b09ac14a6efe14c96eaa6c2f7b493
Artyom Izotov <ezik5353ezik@gmail.com>
2020-05-15 20:05:32 +0300

    Code: Выполнил пункт 3.2.6

--- main.cpp
+++ main.cpp
@@ -87,7 +87,7 @@ void show_histogram_text(vector<size_t>bins)
         {
             cout << '*';
         }
-       cout<<' ';
+        cout<<' ';
         for (size_t i=height; i<max_count+1; i++)
         {
             cout<<" ";
@@ -102,18 +102,28 @@ int main(int argc, char* argv[])
 {
     if (argc>1)
     {
-        cout<<"argv[0]="<<argv[0];
-        return(0);
+        //cout<<"argv[0]="<<argv[0]<<" argc="<<argc;
+        CURL *curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            return(0);
+        }
+
     }
-    else{
-    curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin,true);
-    const auto bins = make_histogram(input);
-    show_histogram_svg(bins);
-    //show_histogram_text(bins);
+
+        curl_global_init(CURL_GLOBAL_ALL);
+        const auto input = read_input(cin,true);
+        const auto bins = make_histogram(input);
+        show_histogram_svg(bins);
+        //show_histogram_text(bins);
 
 
-    return 0;}
+        return 0;
+
 }
 
 
