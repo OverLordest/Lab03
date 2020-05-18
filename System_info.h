2ab789ad1837237569a19fac9e919730ab1729a9
Artyom Izotov <ezik5353ezik@gmail.com>
2020-05-18 16:23:22 +0300

    Code: 3.3.2 небоьшие правки

--- System_info.h
+++ System_info.h
@@ -1,7 +1,4 @@
 #ifndef SYSTEM_INFO_H_INCLUDED
 #define SYSTEM_INFO_H_INCLUDED
-#include <iostream>
-#include <string>
-#include <sstream>
 string make_info_text();
 #endif // SYSTEM_INFO_H_INCLUDED
