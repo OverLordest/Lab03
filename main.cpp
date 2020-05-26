fb80ab2f106616f9dd691f4b62b26b1798e99a12
Artyom Izotov <ezik5353ezik@gmail.com>
2020-05-26 15:20:41 +0300

    Code: Выполнил ИДЗ

--- main.cpp
+++ main.cpp
@@ -6,7 +6,54 @@
 #include "histogram.h"
 #include "histogram_svg.h"
 using namespace std;
+struct Options
+{
+    size_t bins;
+    bool bins_correct;
+    bool use_help;
+    char* url;
+};
+Options parse_args(int argc, char** argv)
+{
+    Options opt;
+    opt.url=0;
+    opt.bins=0;
+    opt.bins_correct=false;
+    opt.use_help = false;
 
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            if (strcmp(argv[i],"-bins") == 0)
+            {
+                if(i+1<argc)
+                {
+                    opt.bins= atoi(argv[i+1]);
+                    if (opt.bins)
+                    {
+                        opt.bins_correct=true;
+                        i++;
+                    }
+                    else
+                        {
+                            opt.use_help=true;
+                        }
+                }
+
+                else
+                {
+                    opt.use_help = true;
+                }
+            }
+        }
+        else
+        {
+            opt.url=argv[i];
+        }
+    }
+    return opt;
+}
 vector<double>
 input_numbers(istream& in,size_t count) {
     vector<double> result(count);
@@ -17,9 +64,18 @@ input_numbers(istream& in,size_t count) {
 }
 
 Input
-read_input(istream& in,bool prompt) {
+read_input(istream& in,bool prompt,const Options &opt) {
     Input data;
     size_t number_count;
+    if (opt.bins_correct)
+    {
+        cerr << "Enter number count: ";
+        in >> number_count;
+        cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
+        data.bin_count=opt.bins;
+        return data;
+    }
     if(prompt)
     {
     cerr << "Enter number count: ";
@@ -50,7 +106,7 @@ write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 }
 
 Input
-download(const string& address) {
+download(const string& address,const Options &opt) {
     stringstream buffer;
 
     curl_global_init(CURL_GLOBAL_ALL);
@@ -69,19 +125,25 @@ download(const string& address) {
         }
     }
    curl_easy_cleanup(curl);
-   return read_input(buffer, false);
+   return read_input(buffer, false, opt);
 }
 
 int
 main(int argc, char* argv[]) {
     Input input;
-   if (argc > 1)
+    Options opt=parse_args(argc,argv);
+    if (opt.use_help)
     {
-        input = download(argv[1]);
+        cerr<<"Error";
+        return 1;
+    }
+   if (opt.url)
+    {
+        input = download(opt.url,opt);
     }
     else
     {
-        input = read_input(cin, true);
+        input = read_input(cin, true, opt);
     }
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
